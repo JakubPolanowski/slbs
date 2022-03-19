7360f1addee3c41b6eccacdc58982a26988c5621
Jakub Polanowski <jakubpol27@gmail.com>
2022-03-19 17:29:49 -0400

    updated imports, naming, and removed hard coded python version requirement

--- slbs/__init__.py
+++ slbs/__init__.py
@@ -1,36 +1,33 @@
-from fbs import _state
-from fbs._state import LOADED_PROFILES
-from fbs_runtime import FbsError, _source
-from fbs_runtime._fbs import get_core_settings, get_default_profiles
-from fbs_runtime._settings import load_settings, expand_placeholders
-from fbs_runtime._source import get_settings_paths
+from slbs import _state
+from slbs._state import LOADED_PROFILES
+from slbs_runtime import slbsError, _source
+from slbs_runtime._slbs import get_core_settings, get_default_profiles
+from slbs_runtime._settings import load_settings, expand_placeholders
+from slbs_runtime._source import get_settings_paths
 from os.path import abspath
 
 import sys
 
 """
-fbs populates SETTINGS with the current build settings. A typical example is
+slbs populates SETTINGS with the current build settings. A typical example is
 SETTINGS['app_name'], which you define in src/build/settings/base.json.
 """
 SETTINGS = _state.SETTINGS
 
+
 def init(project_dir):
     """
-    Call this if you are invoking neither `fbs` on the command line nor
-    fbs.cmdline.main() from Python.
+    Call this if you are invoking neither `slbs` on the command line nor
+    slbs.cmdline.main() from Python.
     """
-    if sys.version_info[0] != 3 or sys.version_info[1] not in (5, 6):
-        raise FbsError(
-            'The free version of fbs only supports Python 3.5 and 3.6.\n'
-            'Please obtain fbs Pro from https://build-system.fman.io/pro.'
-        )
     SETTINGS.update(get_core_settings(abspath(project_dir)))
     for profile in get_default_profiles():
         activate_profile(profile)
 
+
 def activate_profile(profile_name):
     """
-    By default, fbs only loads some settings. For instance,
+    By default, slbs only loads some settings. For instance,
     src/build/settings/base.json and .../`os`.json where `os` is one of "mac",
     "linux" or "windows". This function lets you load other settings on the fly.
     A common example would be during a release, where release.json contains the
@@ -41,6 +38,7 @@ def activate_profile(profile_name):
     json_paths = get_settings_paths(project_dir, LOADED_PROFILES)
     core_settings = get_core_settings(project_dir)
     SETTINGS.update(load_settings(json_paths, core_settings))
+
 
 def path(path_str):
     """
@@ -53,7 +51,7 @@ def path(path_str):
     try:
         project_dir = SETTINGS['project_dir']
     except KeyError:
-        error_message = "Cannot call path(...) until fbs.init(...) has been " \
+        error_message = "Cannot call path(...) until slbs.init(...) has been " \
                         "called."
-        raise FbsError(error_message) from None
+        raise slbsError(error_message) from None
     return _source.path(project_dir, path_str)
