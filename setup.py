4659ac66b480d4742f04f830502fe7dc23bf0cfe
Jakub Polanowski <jakubpol27@gmail.com>
2022-03-19 16:46:24 -0400

    renamed package, changed requirements, updated metadata

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 """Create cross-platform desktop applications with Python and Qt
 
 See:
-https://build-system.fman.io
+https://github.com/JakubPolanowski/slbs  # TODO replace with new wiki once first release ready and wiki added
 """
 
 from os.path import relpath, join
 from setuptools import setup, find_packages
 
 import os
+
 
 def _get_package_data(pkg_dir, data_subdir):
     result = []
@@ -17,53 +18,58 @@ def _get_package_data(pkg_dir, data_subdir):
             result.append(relpath(filepath, pkg_dir))
     return result
 
+
 description = 'Create cross-platform desktop applications with Python and Qt'
 setup(
-    name='fbs',
-    # Also update fbs/_defaults/requirements/base.txt when you change this:
-    version='1.0.3',
+    name='slbs',
+    # Also update slbs/_defaults/requirements/base.txt when you change this:
+    version='0.0.0',
     description=description,
-    long_description=
-        description + '\n\nHome page: https://build-system.fman.io',
-    author='Michael Herrmann',
-    author_email='michael+removethisifyouarehuman@herrmann.io',
-    url='https://build-system.fman.io',
+    long_description=description + \
+    '\n\nHome page: https://github.com/JakubPolanowski/slbs',
+    author='Jakub Polanowski',
+    author_email='jakubpol27@gmail.com',
+    url='https://github.com/JakubPolanowski/slbs',
     packages=find_packages(exclude=('tests', 'tests.*')),
     package_data={
-        'fbs': _get_package_data('fbs', '_defaults'),
-        'fbs.builtin_commands':
-            _get_package_data('fbs/builtin_commands', 'project_template'),
-        'fbs.builtin_commands._gpg':
+        'slbs': _get_package_data('fbs', '_defaults'),
+        'slbs.builtin_commands':
+            _get_package_data('slbs/builtin_commands', 'project_template'),
+        'slbs.builtin_commands._gpg':  # TODO: check if this is required given goals of slbs
             ['Dockerfile', 'genkey.sh', 'gpg-agent.conf'],
-        'fbs.installer.mac': _get_package_data(
-            'fbs/installer/mac', 'create-dmg'
+        'slbs.installer.mac': _get_package_data(
+            'slbs/installer/mac', 'create-dmg'
         )
     },
-    install_requires=['PyInstaller==3.4'],
+    # TODO: explore if version requirement appropriate
+    install_requires=['PyInstaller>=3.4'],
     extras_require={
         # Also update requirements.txt when you change this:
-        'licensing': ['rsa>=3.4.2'],
-        'sentry': ['sentry-sdk>=0.6.6'],
-        'upload': ['boto3']
+        'licensing': ['rsa>=3.4.2'],  # TODO recheck if necessary
+        'sentry': ['sentry-sdk>=0.6.6'],  # TODO recheck if necessary
+        'upload': ['boto3']  # TODO recheck if necessary
     },
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
+        'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
-    
+
         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
-    
+
         'Operating System :: OS Independent',
-    
+
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
 
         'Topic :: Software Development :: Libraries',
         'Topic :: Software Development :: Libraries :: Python Modules'
     ],
     entry_points={
-        'console_scripts': ['fbs=fbs.__main__:_main']
+        'console_scripts': ['slbs=slbs.__main__:_main']
     },
     license='GPLv3 or later',
     keywords='PyQt',
